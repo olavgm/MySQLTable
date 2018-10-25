4a5e3df3b464145266f8619b305aacee73607c70
Olav Gausaker <olav@vibbio.com>
2018-10-25 17:22:06 +0200

    Converted into proper class

--- mysqltable.js
+++ mysqltable.js
@@ -1,158 +1,162 @@
-var db = require('mysql-promise')()
+class MySQLTable {
+  constructor (config, tableName, tableId) {
+    this.tableName = tableName
+    this.tableId = tableId
 
-var connection = {
-  query: function (query, parameterValues) {
-    return db.query.apply(db, arguments).then((rowsAndInfo) => {
-      return rowsAndInfo[0]
+    var db = require('mysql-promise')()
+    db.configure(config)
+
+    this.connection = {
+      query: function (query, parameterValues) {
+        return db.query.apply(db, arguments).then((rowsAndInfo) => {
+          return rowsAndInfo[0]
+        })
+      }
+    }
+
+    console.log('MYSQLTABLE:', this.tableName, this.tableId)
+  }
+
+  read (recordId) {
+    var sql = 'SELECT * FROM ?? WHERE ?? = ?'
+
+    return this.connection.query(sql, [this.tableName, this.tableId, recordId]).then((results) => {
+      if (results && results.length === 1) {
+        return (results[0])
+      } else {
+        return null
+      }
     })
   }
-}
 
-function MySQLTable (config, tableName, tableId) {
-  module.exports.tableName = tableName
-  module.exports.tableId = tableId
-  db.configure(config)
-}
+  create (record) {
+    var sql = 'INSERT INTO ?? ('
 
-module.exports = MySQLTable
+    var parameters = [this.tableName]
 
-MySQLTable.prototype.read = (recordId) => {
-  var sql = 'SELECT * FROM ?? WHERE ?? = ?'
+    var keys = []
+    var values = []
 
-  return connection.query(sql, [module.exports.tableName, module.exports.tableId, recordId]).then((results) => {
-    if (results && results.length === 1) {
-      return (results[0])
-    } else {
-      return null
-    }
-  })
-}
-
-MySQLTable.prototype.create = (record) => {
-  var sql = 'INSERT INTO ?? ('
-
-  var parameters = [module.exports.tableName]
-
-  var keys = []
-  var values = []
-
-  for (var key in record) {
-    if (key !== module.exports.tableId) {
-      keys.push(key)
-      values.push(record[key])
-    }
-  }
-
-  sql += multiple('??', keys.length).join() + ') VALUES (' + multiple('?', values.length).join() + ')'
-
-  parameters = parameters.concat(keys.concat(values))
-
-  return connection.query(sql, parameters).then((result) => {
-    return result.insertId
-  })
-}
-
-MySQLTable.prototype.createMultiple = (records) => {
-  var keys = []
-  var valuesMatrix = []
-
-  var record = records[0]
-
-  var key, i
-
-  for (key in record) {
-    if (key !== module.exports.tableId) {
-      keys.push(key)
-    }
-  }
-
-  var values = []
-
-  for (i = 0; i < records.length; i++) {
-    record = records[i]
-
-    for (key in record) {
-      if (key !== module.exports.tableId) {
+    for (var key in record) {
+      if (key !== this.tableId) {
+        keys.push(key)
         values.push(record[key])
       }
     }
 
-    valuesMatrix.push(values)
+    sql += multiple('??', keys.length).join() + ') VALUES (' + multiple('?', values.length).join() + ')'
+
+    parameters = parameters.concat(keys.concat(values))
+
+    return this.connection.query(sql, parameters).then((result) => {
+      return result.insertId
+    })
   }
 
-  var valuesString = ''
+  createMultiple (records) {
+    var keys = []
+    var valuesMatrix = []
 
-  for (i = 0; i < valuesMatrix.length; i++) {
-    if (valuesString.length > 0) {
-      valuesString += ','
+    var record = records[0]
+
+    var key, i
+
+    for (key in record) {
+      if (key !== this.tableId) {
+        keys.push(key)
+      }
     }
 
-    valuesString += '(' + multiple('?', keys.length).join() + ')'
-  }
+    var values = []
 
-  var sql = 'INSERT INTO ?? ('
-  sql += multiple('??', keys.length).join() + ') VALUES ' + valuesString
+    for (i = 0; i < records.length; i++) {
+      record = records[i]
 
-  var parameters = [module.exports.tableName]
-  parameters = parameters.concat(keys.concat(values))
+      for (key in record) {
+        if (key !== this.tableId) {
+          values.push(record[key])
+        }
+      }
 
-  return connection.query(sql, parameters).then((result) => {
-    return result.affectedRows
-  })
-}
-
-MySQLTable.prototype.update = (record) => {
-  var sql = 'UPDATE ?? SET '
-
-  var parameters = [module.exports.tableName]
-  var queryItems = []
-
-  for (var key in record) {
-    if (key !== module.exports.tableId) {
-      queryItems.push('?? = ?')
-      parameters.push(key, record[key])
+      valuesMatrix.push(values)
     }
+
+    var valuesString = ''
+
+    for (i = 0; i < valuesMatrix.length; i++) {
+      if (valuesString.length > 0) {
+        valuesString += ','
+      }
+
+      valuesString += '(' + multiple('?', keys.length).join() + ')'
+    }
+
+    var sql = 'INSERT INTO ?? ('
+    sql += multiple('??', keys.length).join() + ') VALUES ' + valuesString
+
+    var parameters = [this.tableName]
+    parameters = parameters.concat(keys.concat(values))
+
+    return this.connection.query(sql, parameters).then((result) => {
+      return result.affectedRows
+    })
   }
 
-  sql += queryItems.join() + ' WHERE ?? = ?'
+  update (record) {
+    var sql = 'UPDATE ?? SET '
 
-  parameters.push(module.exports.tableId)
-  parameters.push(record[module.exports.tableId])
+    var parameters = [this.tableName]
+    var queryItems = []
 
-  return connection.query(sql, parameters).then((result) => {
-    return result.affectedRows
-  })
+    for (var key in record) {
+      if (key !== this.tableId) {
+        queryItems.push('?? = ?')
+        parameters.push(key, record[key])
+      }
+    }
+
+    sql += queryItems.join() + ' WHERE ?? = ?'
+
+    parameters.push(this.tableId)
+    parameters.push(record[this.tableId])
+
+    return this.connection.query(sql, parameters).then((result) => {
+      return result.affectedRows
+    })
+  }
+
+  delete (recordId) {
+    var sql = 'DELETE FROM ?? WHERE ?? = ?'
+
+    return this.connection.query(sql, [this.tableName, this.tableId, recordId]).then((result) => {
+      return result.affectedRows
+    })
+  }
+
+  exists (recordId) {
+    var sql = 'SELECT COUNT(*) count FROM ?? WHERE ?? = ?'
+
+    return this.connection.query(sql, [this.tableName, this.tableId, recordId]).then((result) => {
+      return result[0].count >= 1
+    })
+  }
+
+  listAll (limit = 1000, offset = 0) {
+    var sql = 'SELECT * FROM ?? LIMIT ? OFFSET ?'
+
+    return this.connection.query(sql, [this.tableName, limit, offset]).then((result) => {
+      return result
+    })
+  }
+
+  runSql (sql, parameters = []) {
+    return this.connection.query(sql, parameters).then((result) => {
+      return result
+    })
+  }
 }
 
-MySQLTable.prototype.delete = (recordId) => {
-  var sql = 'DELETE FROM ?? WHERE ?? = ?'
-
-  return connection.query(sql, [module.exports.tableName, module.exports.tableId, recordId]).then((result) => {
-    return result.affectedRows
-  })
-}
-
-MySQLTable.prototype.exists = (recordId) => {
-  var sql = 'SELECT COUNT(*) count FROM ?? WHERE ?? = ?'
-
-  return connection.query(sql, [module.exports.tableName, module.exports.tableId, recordId]).then((result) => {
-    return result[0].count >= 1
-  })
-}
-
-MySQLTable.prototype.listAll = (limit = 1000, offset = 0) => {
-  var sql = 'SELECT * FROM ?? LIMIT ? OFFSET ?'
-
-  return connection.query(sql, [module.exports.tableName, limit, offset]).then((result) => {
-    return result
-  })
-}
-
-MySQLTable.prototype.runSql = (sql, parameters = []) => {
-  return connection.query(sql, parameters).then((result) => {
-    return result
-  })
-}
+module.exports = MySQLTable
 
 function multiple (item, times) {
   var result = []
